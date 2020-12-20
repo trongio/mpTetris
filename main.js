6d8da1a71f2b53b32acb86501ee8d73c029e62b7
trongio <giokakabadze50@gmail.com>
2020-12-20 21:15:57 +0400

    made tetris self contained
    now one can create and delete tetris instances

--- main.js
+++ main.js
@@ -1,63 +1,11 @@
-function createPiece(type) {
-    if (type === 'T') {
-        return [
-            [0, 0, 0],
-            [1, 1, 1],
-            [0, 1, 0],
-        ];
-    } else if (type === 'O') {
-        return [
-            [2, 2],
-            [2, 2],
-        ];
-    } else if (type === 'L') {
-        return [
-            [0, 3, 0],
-            [0, 3, 0],
-            [0, 3, 3],
-        ];
-    } else if (type === 'J') {
-        return [
-            [0, 4, 0],
-            [0, 4, 0],
-            [4, 4, 0],
-        ];
-    } else if (type === 'I') {
-        return [
-            [0, 5, 0, 0],
-            [0, 5, 0, 0],
-            [0, 5, 0, 0],
-            [0, 5, 0, 0],
-        ];
-    } else if (type === 'S') {
-        return [
-            [0, 6, 6],
-            [6, 6, 0],
-            [0, 0, 0],
-        ];
-    } else if (type === 'Z') {
-        return [
-            [7, 7, 0],
-            [0, 7, 7],
-            [0, 0, 0],
-        ];
-    }
-}
-
-const tetri = [];
-
-const playerElements = document.querySelectorAll('.player');
-[...playerElements].forEach(element => {
-    const tetris = new Tetris(element);
-    tetri.push(tetris);
-});
-
+const tetrisManager = new TetrisManager(document)
+const localTetris= tetrisManager.createPlayer();
 const keyListener = (event) => {
     [
         [65, 68, 81, 69, 83],
         [72, 75, 89, 73, 74],
     ].forEach((key, index) => {
-        const player = tetri[index].player;
+        const player = localTetris.player;
         if (event.type === 'keydown') {
             if (event.keyCode === key[0]) {
                 player.move(-1);
---
+++ tetris-manager.js
@@ -0,0 +1,29 @@
+class  TetrisManager {
+    constructor(document) {
+        this.document=document;
+
+        this.instances= new Set;
+
+        this.template=document.getElementById('player-template');
+
+        const tetri = [];
+
+    }
+
+    createPlayer(){
+        const element = this.document
+            .importNode(this.template.content,true)
+            .children[0];
+        const tetris = new Tetris(element);
+        this.instances.add(tetris);
+
+        this.document.body.appendChild(tetris.element);
+
+        return tetris;
+    }
+
+    removePlayer(tetris){
+        this.instances.delete(tetris);
+        this.document.body.removeChild(tetris.element);
+    }
+}
